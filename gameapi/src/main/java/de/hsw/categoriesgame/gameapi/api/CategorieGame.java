5d98ce3247aa71fcda0900d7086c1ce7a9448b93
Florian J. Kleine-Vorholt <florian@kleine-vorholt.net>
2024-10-12 19:32:17 +0200

    Added mechanism to check if lobby is full

--- gameapi/src/main/java/de/hsw/categoriesgame/gameapi/api/CategorieGame.java
+++ gameapi/src/main/java/de/hsw/categoriesgame/gameapi/api/CategorieGame.java
@@ -1,6 +1,7 @@
 package de.hsw.categoriesgame.gameapi.api;
 
 import de.hsw.categoriesgame.gameapi.exception.LobbyAlreadyExistsException;
+import de.hsw.categoriesgame.gameapi.exception.LobbyFullException;
 import de.hsw.categoriesgame.gameapi.exception.LobbyNotFoundException;
 import de.hsw.categoriesgame.gameapi.exception.UserNotInLobbyException;
 import de.hsw.categoriesgame.gameapi.pojo.GameConfigs;
@@ -17,6 +18,7 @@ public interface CategorieGame {
      */
     Lobby getLobby(String lobbyCode) throws LobbyNotFoundException;
 
+
     /**
      * Specified player joins a specified lobby.
      * @param lobbyCode string that is specified when creating a lobby
@@ -24,13 +26,8 @@ public interface CategorieGame {
      * @return          object of class Lobby that the user joined
      * @throws LobbyNotFoundException if lobby is not found (e.g. not valid lobbyCode)
      */
-    Lobby joinLobby(String lobbyCode, final Client client) throws LobbyNotFoundException;
+    Lobby joinLobby(String lobbyCode, final Client client) throws LobbyNotFoundException, LobbyFullException;
 
-    /**
-     * Create lobby with random UUID lobby code.
-     * @return  object of class lobby that was created
-     */
-    Lobby createLobby();
 
     /**
      * Create lobby with specified unique lobby code.
@@ -39,11 +36,6 @@ public interface CategorieGame {
      */
     Lobby createLobby(String lobbyCode, GameConfigs gameConfigs) throws LobbyAlreadyExistsException;
 
-    /**
-     * Get all created lobbies.
-     * @return  list of all created lobbies
-     */
-    List<Lobby> getLobbies();
 
     /**
      * Deletes specified lobby.
@@ -52,12 +44,14 @@ public interface CategorieGame {
      */
     void deleteLobby(String lobbyCode) throws LobbyNotFoundException, UserNotInLobbyException;
 
+
     /**
      * Deletes specified lobby.
      * @param lobby object of class Lobby that should be deleted
      * @throws LobbyNotFoundException if lobby is not found (e.g. not valid lobbyCode)
      */
     void deleteLobby(Lobby lobby) throws LobbyNotFoundException;
+
 
     /**
      * Specified player leaves the specified lobby.
@@ -67,6 +61,7 @@ public interface CategorieGame {
      */
     void leaveLobby(String lobbyCode, Client client) throws LobbyNotFoundException, UserNotInLobbyException;
 
+
     /**
      * List of players leave the specified lobby.
      * @param lobbyCode string that identifies the lobby
@@ -75,12 +70,14 @@ public interface CategorieGame {
      */
     void leaveLobby(String lobbyCode, List<Client> clients) throws LobbyNotFoundException, UserNotInLobbyException;
 
+
     /**
      * Specified player leaves the specified lobby.
      * @param lobby     object of class Lobby
      * @param client    object of class Player
      */
     void leaveLobby(Lobby lobby, Client client) throws LobbyNotFoundException, UserNotInLobbyException;
+
 
     /**
      * Specified list of players leave the specified lobby.
